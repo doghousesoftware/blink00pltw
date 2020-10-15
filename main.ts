3f554a797156ad33f91371a79f2bcb95f904e1a4
bryan weinstein <bryanweinstein@outlook.com>
2020-10-14 18:07:13 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,76 @@
+basic.forever(function () {
+    basic.showString("Bus")
+    basic.showLeds(`
+        . . . . .
+        # # # # .
+        # . . . #
+        # # # # #
+        . # . # .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # # . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        # # # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . . # . .
+        # # # # .
+        . . # . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . . #
+        . # # # #
+        . . . . #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . . .
+        . . # # #
+        . . . . .
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . # #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+})
