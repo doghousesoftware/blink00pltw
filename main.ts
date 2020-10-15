94eddf7d893d918a509e34008906bfd3ae457790
bryan weinstein <bryanweinstein@outlook.com>
2020-10-14 18:15:57 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,76 +1,78 @@
 basic.forever(function () {
-    basic.showString("Bus")
-    basic.showLeds(`
-        . . . . .
-        # # # # .
-        # . . . #
-        # # # # #
-        . # . # .
-        `)
-    basic.pause(500)
-    basic.clearScreen()
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        # . . . .
-        # # . . .
-        # . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        # . . . .
-        . # . . .
-        # # # . .
-        . # . . .
-        # . . . .
-        `)
-    basic.showLeds(`
-        . # . . .
-        . . # . .
-        # # # # .
-        . . # . .
-        . # . . .
-        `)
-    basic.showLeds(`
-        . . # . .
-        . . . # .
-        # # # # #
-        . . . # .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . . . # .
-        . . . . #
-        . # # # #
-        . . . . #
-        . . . # .
-        `)
-    basic.showLeds(`
-        . . . . #
-        . . . . .
-        . . # # #
-        . . . . .
-        . . . . #
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . # #
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . #
-        . . . . .
-        . . . . .
-        `)
-    basic.clearScreen()
+    if (input.compassHeading() >= 270 && input.compassHeading() <= 360) {
+        basic.showString("Bus")
+        basic.showLeds(`
+            . . . . .
+            # # # # .
+            # . . . #
+            # # # # #
+            . # . # .
+            `)
+        basic.pause(500)
+        basic.clearScreen()
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            # # . . .
+            # . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            # # # . .
+            . # . . .
+            # . . . .
+            `)
+        basic.showLeds(`
+            . # . . .
+            . . # . .
+            # # # # .
+            . . # . .
+            . # . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . . . # .
+            . . . . #
+            . # # # #
+            . . . . #
+            . . . # .
+            `)
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # # #
+            . . . . .
+            . . . . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . # #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . .
+            . . . . .
+            `)
+        basic.clearScreen()
+    }
 })
